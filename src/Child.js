cc2768b028cca062b557d71a0c504ccdca00e081
Samuel Muigai <ndungu.muigai01@gmail.com>
2023-11-11 17:21:22 +0300

    Lab code along done

--- src/Child.js
+++ src/Child.js
@@ -1,8 +1,21 @@
 import React from "react";
 import { getRandomColor } from "./randomColorGenerator.js";
 
-function Child({ onChangeColor }) {
-  return <div className="child" style={{ backgroundColor: "#FFF" }} />;
+/*Destructuring the passed in props
+
+onChangeColor-> refers to the click function defined in the Parent component and was passed in as a prop
+color-> the color generated when the Child component is clicked*/
+function Child({ onChangeColor, color }) 
+{
+  //Creating a handleClick function that generates a new color and passes the new color as an argument to the handleChangeColor function that handles setting the background color
+  function handleClick()
+  {
+    const newColor=getRandomColor()
+    //Passing the newly generated color to the onChangeColor function
+    onChangeColor(newColor)
+  }
+
+  return <div onClick={handleClick} className="child" style={{ backgroundColor: color }} />;
 }
 
 export default Child;
--- src/Parent.js
+++ src/Parent.js
@@ -4,12 +4,27 @@ import Child from "./Child";
 
 function Parent() {
   const randomColor = getRandomColor();
+
+  //Creating a state variable to keep track of the color of the Parent component
   const [color, setColor] = useState(randomColor);
+
+  //Creating a state variable to keep track of the color of the Child components
+  const [childrenColor, setChildrenColor]=useState("#FFF")
+
+  /*Defining a function that updates the states created above
+  It takes in an argument passed in from the child components. The argument is used to update the state of the Child compinent's color variable*/
+  function handleChangeColor (newChildColor) 
+  {
+    const newRandomColor=getRandomColor()
+    setColor(newRandomColor)//Updates the color state
+    setChildrenColor(newChildColor)//Updates the state of the Child component color
+  }
 
   return (
     <div className="parent" style={{ backgroundColor: color }}>
-      <Child />
-      <Child />
+      {/* Passing on the color and function that handles clicking of the child components */}
+      <Child color={childrenColor} onChangeColor={handleChangeColor}/>
+      <Child color={childrenColor} onChangeColor={handleChangeColor}/>
     </div>
   );
 }
